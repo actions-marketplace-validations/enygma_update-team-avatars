161002da9ff6b7c3eaccb49ebfd07f767536354a
Chris Cornutt <enygma@github.com>
2021-10-19 13:49:38 -0600

    Removing extra output from entrypoint

--- entrypoint.sh
+++ entrypoint.sh
@@ -11,8 +11,6 @@ cd fetch-team-avatars
 # Run the command to get the org data
 php ./run.php --GH_TOKEN=$1 --GH_USER=$3 --GH_TEAM_URL=$4 > out.txt
 
-# cat out.txt
-
 # A bit of config
 git config --global user.email "$5"
 git config --global user.name "$3"
@@ -23,9 +21,7 @@ cd tmp
 git checkout $5
 
 # Perform the replacement
-echo "Replacement"
 sed -i "s/<TEAM>/$(sed -e 's/[\&/]/\\&/g' -e 's/$/\\n/' ../out.txt | tr -d '\n')/g" README.md
-cat README.md
 
 # Commit and push!
 git add README.md; git commit -m'Team avatar update'; git push origin $5
